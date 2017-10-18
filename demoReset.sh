a8a147131abf442a6f652bfd5e347181f342e6c5
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-10-18 14:51:36 -0500

    ready for communities

---
+++ demoReset.sh
@@ -0,0 +1,10 @@
+# remove all the data the demo creates
+sfdx force:apex:execute -f scripts/demoReset.cls
+
+# let's build a destructiveChanges.xml file
+# touch destructiveChanges.xml
+# find the tab created that's from appBuilder, add to file
+
+# find the appbuilder app, add to file
+
+# deploy via mdapi
